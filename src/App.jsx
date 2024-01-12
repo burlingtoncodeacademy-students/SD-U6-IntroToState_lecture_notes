67155f8d3ace97fbfb92f4e42b820131411e70f2
Eric Winebrenner <ejwinebrenner@gmail.com>
2024-01-12 12:33:44 -0500

    notes updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
+import { useState } from 'react';
 import './App.css';
 import Welcome from './components/welcome/Welcome';
 
 function App() {
 
-  const name = "Sam"
+  const [ names, setNames ] = useState([
+    'Frodo', 'Sam', 'Pippin', 'Merry'
+  ]);
+
+  const displayWelcome = () => {
+    return(names.map((name, index) => {
+      return(
+        <Welcome 
+          key={index}
+          name={name}
+          names={names} // obtaining our full array in the current state
+          setName={setNames} // passing our function so that we may change the state
+        />
+      )
+    }))
+  }
 
   return (
     <div className="App">
-      <Welcome name={name}/>
+      {displayWelcome()}
     </div>
   );
 }
--- src/components/welcome/Welcome.jsx
+++ src/components/welcome/Welcome.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
 
 function Welcome(props) {
+
+    // console.log(props)
+
+    function removeName() {
+        // console.log('Clicked!')
+
+        const updatedNames = props.names.filter(name => name !== props.name);
+
+        props.setName(updatedNames);
+    }
+
     return (
-        <div className='welcome'>Welcome, {props.name}</div>
+        <div onClick={removeName} className='welcome'>Welcome, {props.name}</div>
     )
 }
 
